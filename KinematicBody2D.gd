f339452579f651cd3add9dc4a04047ba0d94767c
eugene-nguyen <57508535+eugene-nguyen@users.noreply.github.com>
2020-03-23 14:59:34 -0700

    asdf i messed up the git setup earlier

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,39 +1,26 @@
 extends KinematicBody2D
 # This script controls movement.
 
-var motion = Vector2()
-var speed = 0
-var angle = 0
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5
 
-var speed_accel = 5
-var angle_accel = 1
-var min_speed = -500
-var max_speed = 500
-var max_angle = 2 * PI
+var velocity = Vector2()
+var rotation_dir = 0
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		rotation_dir += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_dir -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed('ui_up'):
+		velocity = Vector2(speed, 0).rotated(rotation)
+
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"):
-		speed = min(speed_accel + speed, max_speed)
-	elif Input.is_action_pressed("ui_down"):
-		speed = max(speed_accel - speed, min_speed)
-	else:
-		while (speed > 0):
-			speed -= speed_accel
-		while (speed < 0):
-			speed += speed_accel
-			
-	
-	if Input.is_action_pressed("ui_left"):
-		angle -= angle_accel
-	elif Input.is_action_pressed("ui_right"):
-		angle += angle_accel
-	
-	angle = angle % 360
-	
-	motion.x = speed * cos(angle)
-	motion.y = speed * sin(angle)
-	
-	print(speed)
-	print(angle)
-	
-	move_and_slide(motion)
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
