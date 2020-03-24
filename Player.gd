fb912ef2cde930efbc93dbd6e1f2072027035e6e
eugene-nguyen <57508535+eugene-nguyen@users.noreply.github.com>
2020-03-24 15:26:09 -0700

    SHOOTING!

--- Player.gd
+++ Player.gd
@@ -3,9 +3,10 @@ extends KinematicBody2D
 export (int) var turn_speed = 180
 export (int) var move_speed = 150
 export (float) var acceleration = 0.05
-export (float) var deceleration = 0.01
+export (float) var deceleration = 0.0125
 
 var motion = Vector2(0, 0)
+var Bullet = preload("res://Bullet.tscn")
 
 var screen_size
 var screen_buffer = 8
@@ -18,10 +19,16 @@ func get_input(delta, move_direction):
 	
 	if Input.is_action_pressed("ui_up"):
 		motion = motion.linear_interpolate(move_direction, acceleration)
-	elif Input.is_action_pressed("ui_down"):
-		motion = motion.linear_interpolate(-move_direction, acceleration)
 	else:
 		motion = motion.linear_interpolate(Vector2(0, 0), deceleration)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		shoot(move_direction)
+
+func shoot(move_direction):
+	var b = Bullet.instance()
+	b.start(position, rotation)
+	get_parent().add_child(b)
 
 func _ready():
 	screen_size = get_viewport_rect().size
