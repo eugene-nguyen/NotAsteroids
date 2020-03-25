2392302b010ba6df178860dc117f3a16c118fe2b
eugene-nguyen <57508535+eugene-nguyen@users.noreply.github.com>
2020-03-24 19:20:08 -0700

    modified Bullet so they now delete themselves upon collision with "Enemy"
    well, only the very first one

--- Bullet.gd
+++ Bullet.gd
@@ -11,9 +11,10 @@ func start(pos, dir):
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		velocity = velocity.bounce(collision.normal)
-		if collision.collider.has_method("hit"):
-			collision.collider.hit()
-
+		print(collision.collider.name)
+		if collision.collider.get_node("Enemy"):
+			hide()
+			queue_free()
+	
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
